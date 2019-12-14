1ceec69b434ca2caac898e67131e0f4920509f31
Andrei-Sebastian <Andrei-Sebastian@users.noreply.github.com>
2019-12-14 19:06:27 +0200

    Masina+Assert

---
+++ Taxi Simulator/Assets/Scripts/SimpleCarController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCarController : MonoBehaviour {
+
+	public void GetInput()
+	{
+		m_horizontalInput = Input.GetAxis("Horizontal");
+		m_verticalInput = Input.GetAxis("Vertical");
+      //  m_spaceInput = Input.GetAxis("Space");
+    }
+
+	private void Steer()
+	{
+		m_steeringAngle = maxSteerAngle * m_horizontalInput;
+		frontDriverW.steerAngle = m_steeringAngle;
+		frontPassengerW.steerAngle = m_steeringAngle;
+	}
+
+	private void Accelerate()
+	{
+		frontDriverW.motorTorque = m_verticalInput * motorForceFront;
+		frontPassengerW.motorTorque = m_verticalInput * motorForceBack;
+        
+	}
+
+	private void UpdateWheelPoses()
+	{
+		UpdateWheelPose(frontDriverW, frontDriverT);
+		UpdateWheelPose(frontPassengerW, frontPassengerT);
+		UpdateWheelPose(rearDriverW, rearDriverT);
+		UpdateWheelPose(rearPassengerW, rearPassengerT);
+	}
+
+	private void UpdateWheelPose(WheelCollider _collider, Transform _transform)
+	{
+		Vector3 _pos = _transform.position;
+		Quaternion _quat = _transform.rotation;
+
+		_collider.GetWorldPose(out _pos, out _quat);
+
+		_transform.position = _pos;
+		_transform.rotation = _quat;
+	}
+
+	private void FixedUpdate()
+	{
+		GetInput();
+		Steer();
+		Accelerate();
+		UpdateWheelPoses();
+	}
+
+	private float m_horizontalInput;
+	private float m_verticalInput;
+	private float m_steeringAngle;
+
+	public WheelCollider frontDriverW, frontPassengerW;
+	public WheelCollider rearDriverW, rearPassengerW;
+	public Transform frontDriverT, frontPassengerT;
+	public Transform rearDriverT, rearPassengerT;
+	public float maxSteerAngle = 30;
+	public float motorForceFront = 500;
+    public float motorForceBack = 50;
+    public float m_spaceInput = 0;
+}
